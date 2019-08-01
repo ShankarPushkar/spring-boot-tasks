a15e02c6ae55a287b20a999e32da3c9231479efe
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-01 15:48:26 +0530

    Initial Commit

---
+++ src/main/java/com/stackroute/domain/Track.java
@@ -0,0 +1,48 @@
+package com.stackroute.domain;
+
+public class Track {
+
+    private int id;
+    private String trackName;
+    private String comment;
+    //Constructor
+
+    public Track(int id, String trackName, String comment) {
+        this.id = id;
+        this.trackName = trackName;
+        this.comment = comment;
+    }
+//Getter and Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+// toString
+    @Override
+    public String toString() {
+        return "Track{" +
+                "id=" + id +
+                ", trackName='" + trackName + '\'' +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+}
