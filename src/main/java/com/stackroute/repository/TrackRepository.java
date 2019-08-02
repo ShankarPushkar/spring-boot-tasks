ba31818e1dac3d4e680b9b96d5fddfb13722c7c0
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-02 12:20:15 +0530

    Added Query in TrackRepository

--- src/main/java/com/stackroute/repository/TrackRepository.java
+++ src/main/java/com/stackroute/repository/TrackRepository.java
@@ -2,9 +2,13 @@ package com.stackroute.repository;
 
 import com.stackroute.domain.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface TrackRepository extends JpaRepository<Track,Integer> {
+import java.util.List;
 
+@Repository
+public interface TrackRepository extends JpaRepository<Track, Integer> {
+    @Query("select track from Track track where track.name like ?1")
+    List<Track> getTrackByName(String name);
 }
