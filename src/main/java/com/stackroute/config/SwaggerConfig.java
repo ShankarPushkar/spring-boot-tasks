ec010a397dfe5b080290671d17f1185447c9c987
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-05 18:02:50 +0530

    Added concise SwaggerConfig Code

--- src/main/java/com/stackroute/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/config/SwaggerConfig.java
@@ -2,21 +2,19 @@ package com.stackroute.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    /*    creating a docket bean
+    Docket is intended to be the primary interface into the swagger-spring framework.
+    It Provides sensible defaults and convenience methods for configuration.
+     */
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("com.stackroute"))
-                .paths(regex("/api/v1.*"))
-                .build();
+    public Docket trackApi() {
+        return new Docket(DocumentationType.SWAGGER_2);
     }
 }
