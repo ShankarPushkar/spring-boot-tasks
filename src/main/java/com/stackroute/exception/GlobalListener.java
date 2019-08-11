bd5298549d86edb59cdab2475459e4ac3c6925c8
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-11 09:20:30 +0530

    Modified TrackService Implementation

--- src/main/java/com/stackroute/exception/GlobalListener.java
+++ src/main/java/com/stackroute/exception/GlobalListener.java
@@ -1,6 +1,5 @@
 package com.stackroute.exception;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,7 +9,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class GlobalListener extends ResponseEntityExceptionHandler {
-//    @ExceptionHandler(Exception.class)
+    //    @ExceptionHandler(Exception.class)
 //    protected ResponseEntity<Object> handleConflict(
 //            RuntimeException ex, WebRequest request) {
 //        return new ResponseEntity<>(ex.getMessage(),HttpStatus.CONFLICT);
@@ -26,12 +25,12 @@ public class GlobalListener extends ResponseEntityExceptionHandler {
 //            RuntimeException ex, WebRequest request) {
 //        return new ResponseEntity<>(ex.getMessage(),HttpStatus.CONFLICT);
 //    }
-@ExceptionHandler(value
-        = {TrackAlreadyExistException.class, TrackNotFoundException.class})
-protected ResponseEntity<Object> handleConflict(
-        Exception ex, WebRequest request) {
-    String bodyOfResponse = "This class is for the global exception controller";
-    return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
-}
+    @ExceptionHandler(value
+            = {TrackAlreadyExistException.class, TrackNotFoundException.class})
+    protected ResponseEntity<Object> handleConflict(
+            Exception ex, WebRequest request) {
+        String bodyOfResponse = "This class is for the global exception controller";
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
 }
 
